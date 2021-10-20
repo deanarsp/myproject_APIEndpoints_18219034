7123a39a894b98d643cfaa4b2013b7cee49cabff
deanarsp <deanarsp@gmail.com>
2021-10-20 09:09:46 +0700

    merged second link with tugas endpoint

--- app/api.py
+++ app/api.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, Body, Depends
+import json
 
-from app.model import PostSchema, UserSchema, UserLoginSchema
+from fastapi import FastAPI, HTTPException, Body, Depends
+
+from app.model import MenuSchema, UserSchema, UserLoginSchema
 from app.auth.auth_bearer import JWTBearer
 from app.auth.auth_handler import signJWT
+
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 posts = [
     {
@@ -14,39 +19,85 @@ posts = [
 
 users = []
 
+with open("menu.json", "r") as read_file:
+	menu_data = json.load(read_file)
+    
 app = FastAPI()
 
-
-@app.get("/", tags=["root"])
-async def read_root() -> dict:
-    return {"message": "Welcome to your blog!."}
-
-
-@app.get("/posts", tags=["posts"])
-async def get_posts() -> dict:
-    return { "data": posts }
+@app.get("/", dependencies=[Depends(JWTBearer())], tags=["root"])
+async def root() -> dict:
+	return{"Menu":"Item"}
 
 
-@app.get("/posts/{id}", tags=["posts"])
-async def get_single_post(id: int) -> dict:
-    if id > len(posts):
-        return {
-            "error": "No such post with the supplied ID."
-        }
+@app.get("/menu", dependencies=[Depends(JWTBearer())], tags=["menu"])
+async def read_all_menu() -> dict:
+	return menu_data
 
-    for post in posts:
-        if post["id"] == id:
-            return {
-                "data": post
-            }
+@app.get("/menu/{item_id}", dependencies=[Depends(JWTBearer())], tags=["menu"])
+async def read_menu(item_id: int) -> dict:
+	for menu_item in menu_data["menu"]:
+		if menu_item["id"] == item_id:
+			return menu_item
+	raise HTTPException(
+		status_code=404, detail=f"Item not found"
+		)
 
-@app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
-async def add_post(post: PostSchema) -> dict:
-    post.id = len(posts) + 1
-    posts.append(post.dict())
-    return {
-        "data": "post added."
-    }
+@app.post("/menu", dependencies=[Depends(JWTBearer())], tags=["menu"])
+async def add_menu(menu: MenuSchema) -> dict:
+	id=1
+	if (len(menu_data["menu"])>0):
+		id = menu_data["menu"][len(menu_data["menu"])-1]["id"]+1
+	new_data = {"id":id, "name":menu.name}
+	menu_data['menu'].append(dict(new_data))
+	read_file.close()
+	with open("menu.json", "w") as write_file:
+		json.dump(menu_data,write_file,indent=4)
+	write_file.close()
+
+	return (new_data)
+	raise HTTPException(
+		status_code=500, detail=f"Internal Server Error"
+		)
+
+@app.put("/menu/{item_id}", dependencies=[Depends(JWTBearer())], tags=["menu"])
+async def update_menu(menu: MenuSchema):
+	for menu_item in menu_data["menu"]:
+		if menu_item["id"] == menu.item_id:
+			menu_item["name"] = menu.name
+			read_file.close()
+			with open("menu.json", "w") as write_file:
+				json.dump(menu_data,write_file,indent=4)
+			write_file.close()
+
+			return{"message":"Data updated successfully"}
+
+	raise HTTPException(
+		status_code=404, detail=f"Item not found"
+		)
+
+@app.delete("/menu/{item_id}", dependencies=[Depends(JWTBearer())], tags=["menu"])
+async def delete_menu(item_id: int):
+	for menu_item in menu_data["menu"]:
+		if menu_item["id"] == item_id:
+			menu_data["menu"].remove(menu_item)
+			read_file.close()
+			with open("menu.json", "w") as write_file:
+				json.dump(menu_data,write_file,indent=4)
+			write_file.close()
+
+			return{"message":"Data deleted successfully"}
+
+	raise HTTPException(
+		status_code=404, detail=f"Item not found"
+		)
+
+# @app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
+# async def add_post(post: PostSchema) -> dict:
+#     post.id = len(posts) + 1
+#     posts.append(post.dict())
+#     return {
+#         "data": "post added."
+#     }
 
 @app.post("/user/signup", tags=["user"])
 async def create_user(user: UserSchema = Body(...)):
@@ -66,4 +117,3 @@ async def user_login(user: UserLoginSchema = Body(...)):
     return {
         "error": "Wrong login details!"
     }
-
--- app/model.py
+++ app/model.py
@@ -1,16 +1,15 @@
+from os import name
 from pydantic import BaseModel, Field, EmailStr
 
 
-class PostSchema(BaseModel):
+class MenuSchema(BaseModel):
     id: int = Field(default=None)
-    title: str = Field(...)
-    content: str = Field(...)
+    name: str = Field(...)
 
     class Config:
         schema_extra = {
             "example": {
-                "title": "Securing FastAPI applications with JWT.",
-                "content": "In this tutorial, you'll learn how to secure your application by enabling authentication using JWT. We'll be using PyJWT to sign, encode and decode JWT tokens...."
+                "name": "cheez",
             }
         }
 
