bb8ca1378b24700efd931bce1bcb78530601a227
deanarsp <deanarsp@gmail.com>
2021-10-20 08:41:57 +0700

    trying the second link

--- main.py
+++ main.py
@@ -2,15 +2,3 @@ import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("app.api:app", host="0.0.0.0", port=8081, reload=True)
-
-from app.model import PostSchema
-
-posts = [
-    {
-        "id": 1,
-        "title": "Pancake",
-        "content": "Lorem Ipsum ..."
-    }
-]
-
-users = []
